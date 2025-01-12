ef7885cc9045eedf3bc43277b9c3e4352a73dbb0
KJi0 <165020718+KJi0@users.noreply.github.com>
2025-01-12 19:48:07 +0900

    BOJ_2439

--- 단계별로_풀어보기/BOJ_2439.java
+++
@@ -1,5 +0,0 @@
-package 단계별로_풀어보기;
-
-public class BOJ_2439 {
-
-}
---
+++ 단계별로_풀어보기/반복문/BOJ_2439.java
@@ -0,0 +1,30 @@
+package 단계별로_풀어보기.반복문;
+
+/**
+ * BOJ 2439 : 별 찍기 - 2
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2439 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++)
+				sb.append(' ');
+			for (int k = 0; k < i; k++)
+				sb.append('*');
+	
+			sb.append('\n');
+		}
+		System.out.println(sb);
+	}
+}
