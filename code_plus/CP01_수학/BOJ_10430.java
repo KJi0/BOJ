c983fb7d664d5073083aec578482a922211d908e
KJi0 <165020718+KJi0@users.noreply.github.com>
2025-01-20 00:04:21 +0900

    250119

--- code_plus/CP01_수학/BOJ_10430.java
+++ code_plus/CP01_수학/BOJ_10430.java
@@ -1,9 +1,30 @@
 package code_plus.CP01_수학;
 
+/*
+ * BOJ 10430 : 나머지
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_10430 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		
+		sb.append((A + B) % C).append('\n');
+		sb.append(((A % C) + (B % C)) % C).append('\n');
+		sb.append((A * B) % C).append('\n');
+		sb.append(((A % C) * (B % C)) % C).append('\n');
+		
+		System.out.print(sb);
 	}
-
 }
