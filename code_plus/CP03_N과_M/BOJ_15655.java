55c5db34e6a028d6d1d4257f38c9a898be96cd46
KJi0 <165020718+KJi0@users.noreply.github.com>
2025-02-13 22:14:34 +0900

    250213

---
+++ code_plus/CP03_N과_M/BOJ_15655.java
@@ -0,0 +1,56 @@
+package code_plus.CP03_N과_M;
+
+/*
+ * BOJ 15655 : N과 M (6)
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class BOJ_15655 {
+	public static int N;
+	public static int M;
+	public static int X[];
+	public static int arr[];
+	public static boolean visit[];
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		X = new int[N];
+		arr = new int[M];
+		visit = new boolean[N];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for (int i = 0; i < N; i++)
+			X[i] = Integer.parseInt(st.nextToken());
+		
+		Arrays.sort(X);
+		
+		dfs(0, 0);
+		System.out.println(sb);
+	}
+
+	public static void dfs(int depth, int start) {
+		if (depth == M) {
+			for (int val : arr) {
+				sb.append(val).append(' ');
+				}
+			sb.append('\n');
+			return;
+		}
+		
+		for (int i = start; i < N; i++) {
+			arr[depth] = X[i];
+			dfs(depth + 1, i + 1);
+		}
+	}
+}
