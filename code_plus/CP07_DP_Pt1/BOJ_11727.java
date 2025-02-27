7ef67505aeb5a3f9064acaada04dbbde371052f0
KJi0 <165020718+KJi0@users.noreply.github.com>
2025-02-27 23:26:25 +0900

    250227

--- code_plus/CP07_DP_Pt1/BOJ_11727.java
+++ code_plus/CP07_DP_Pt1/BOJ_11727.java
@@ -1,24 +1,26 @@
 package code_plus.CP07_DP_Pt1;
 
 /*
- * BOJ 11726 : 2xN 타일링
+ * BOJ 11727 : 2xN 타일링 (2)
  */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BOJ_11726 {
+public class BOJ_11727 {
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
 		
+		int N = Integer.parseInt(br.readLine());
 		int[] dp = new int[1001];
+		
 		dp[1] = 1;
-		dp[2] = 2;
+		dp[2] = 3;
 		
 		for (int i = 3; i <= N; i++) {
-			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+			dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
 		}
 		
 		System.out.print(dp[N]);
