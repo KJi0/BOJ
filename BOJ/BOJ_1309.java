202d71dc44a48bd936499fae6dade59f53630b13
KJi0 <165020718+KJi0@users.noreply.github.com>
2025-10-18 14:39:46 +0900

    251018

---
+++ BOJ/BOJ_1309.java
@@ -0,0 +1,29 @@
+package BOJ;
+
+/*
+BOJ 1309: 동물원
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1309 {
+    final static int MOD = 9901;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N][3];
+
+        dp[0][0] = dp[0][1] = dp[0][2] = 1;
+
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % MOD;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
+        }
+
+        System.out.println((dp[N - 1][0] + dp[N - 1][1] + dp[N - 1][2]) % MOD);
+    }
+}
