d447400dd8efae481e7ebd15a40b5456f7277343
KJi0 <165020718+KJi0@users.noreply.github.com>
2025-10-20 20:51:43 +0900

    251020

---
+++ BOJ/BOJ_11057.java
@@ -0,0 +1,32 @@
+package BOJ;
+
+/*
+BOJ 11057: 오르막 수
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11057 {
+    static int MOD = 10_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] dp = new int[N + 1][10];
+
+        for (int i = 0; i < 10; i++) dp[0][i] = 1;
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < 10; j++) {
+                for (int k = j; k < 10; k++) {
+                    dp[i][j] += dp[i - 1][k];
+                    dp[i][j] %= MOD;
+                }
+            }
+        }
+
+        System.out.println(dp[N][0] % MOD);
+    }
+}
