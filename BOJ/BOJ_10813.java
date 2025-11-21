fdacb12ca239907ca450d61c1ec8f268a91d2a43
KJi0 <165020718+KJi0@users.noreply.github.com>
2025-11-21 18:55:12 +0900

    251121

---
+++ BOJ/BOJ_10813.java
@@ -0,0 +1,43 @@
+package BOJ;
+
+/*
+BOJ 10813: 공 바꾸기
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10813 {
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = i;
+        }
+
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            swap(i, j);
+        }
+
+        for (int i = 1; i <= N; i++)
+            System.out.print(arr[i] + " ");
+    }
+
+    static void swap(int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
