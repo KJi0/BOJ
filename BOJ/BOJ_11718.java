6b5bfed0798d4d3b0a4697fdc52f93ba89cdca34
KJi0 <165020718+KJi0@users.noreply.github.com>
2025-11-26 19:03:16 +0900

    251126

---
+++ BOJ/BOJ_11718.java
@@ -0,0 +1,20 @@
+package BOJ;
+
+/*
+BOJ 11718: 그대로 출력하기
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+    }
+}
