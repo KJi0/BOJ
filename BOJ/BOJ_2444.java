28003469bba60a45c16adfa3b2f1f0acd9d5de2b
KJi0 <165020718+KJi0@users.noreply.github.com>
2025-11-28 01:15:27 +0900

    251127

---
+++ BOJ/BOJ_2444.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+/*
+BOJ 2444: 별 찍기 - 7
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2444 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N - i; j++)
+                sb.append(" ");
+            for (int j = 0; j < 2 * i - 1; j++)
+                sb.append("*");
+            sb.append("\n");
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = N - i; j > 0; j--)
+                sb.append(" ");
+            for (int j = 2 * i - 1; j > 0; j--)
+                sb.append("*");
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
