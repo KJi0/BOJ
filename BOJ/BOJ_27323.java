af9449d3cc454dcb2a325f4f3c6b9743ced721ff
KJi0 <165020718+KJi0@users.noreply.github.com>
2025-12-18 23:57:50 +0900

    251218

---
+++ BOJ/BOJ_27323.java
@@ -0,0 +1,19 @@
+package BOJ;
+
+/*
+BOJ 27323: 직사각형
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_27323 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.println(A * B);
+    }
+}
