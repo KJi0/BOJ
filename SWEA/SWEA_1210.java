76abd9b1f95d89ccbedc3453ead6986e3a7dd2bf
Jiyoung Kwak <165020718+KJi0@users.noreply.github.com>
2026-02-04 17:42:22 +0900

    260204

---
+++ SWEA/SWEA_1210.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/*
+ * SWEA 1210: Ladder1
+ * 한 방향으로만 탐색하므로 break 써야 함
+ */
+
+public class Solution_1210_곽지영 {
+	static Node end;
+	static int[][] map;
+	
+	static int[] dx = {0, 0, -1};
+	static int[] dy = {-1, 1, 0};
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		for (int t = 1; t <= 10; t++) {
+			br.readLine();
+			
+			map = new int[100][100];
+			
+			for (int i = 0; i < 100; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < 100; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					
+					if (i == 99 && map[99][j] == 2) end = new Node(99, j);
+				}
+			}
+			
+			System.out.println("#" + t + " " + bfs(end.x, end.y));
+		}
+	}
+	
+	public static int bfs(int x, int y) {
+		Queue<Node> q = new ArrayDeque<>();
+		q.add(new Node(x, y));
+		
+		while (!q.isEmpty()) {
+			Node cur = q.poll();
+			
+			if (cur.x == 0) return cur.y;
+			
+			for (int d = 0; d < 3; d++) {
+				int nx = cur.x + dx[d];
+				int ny = cur.y + dy[d];
+				
+				if (nx < 0 || nx >= 100 || ny < 0 || ny >= 100) continue;
+				if (map[nx][ny] != 1) continue;
+				
+				map[nx][ny] = 3;
+				q.add(new Node(nx, ny));
+				break;
+			}
+		}
+		
+		return 0;
+	}
+	
+	static class Node {
+		int x;
+		int y;
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
---
+++ SWEA/SWEA_6808.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution_6808_곽지영 {
+	/*
+	 * SWEA 6808: 규영이와 인영이의 카드게임
+	 */
+	
+	static boolean[] visited;
+	static int[] gyCard;
+	static int[] iyCard;
+	
+	static int gyScore, iyScore;
+	static int gyWin = 0;
+	static int iyWin = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			gyWin = iyWin = 0;
+			
+			gyCard = new int[10];
+			iyCard = new int[10];
+			visited = new boolean[10];
+			
+			boolean[] check = new boolean[19];
+			
+			for (int i = 0; i < 9; i++) {
+				gyCard[i] = Integer.parseInt(st.nextToken());
+				check[gyCard[i]] = true;
+			}
+			
+			int idx = 0;
+			for (int i = 1; i <= 18; i++) {
+				if (check[i]) continue;
+				iyCard[idx++] = i;
+			}
+			
+			dfs(0, 0, 0);
+			System.out.println("#" + t + " " + gyWin + " " + iyWin);
+		}
+	}
+	
+	public static void dfs(int depth, int gyScore, int iyScore) {
+		if (depth == 9) {
+			if (gyScore > iyScore) gyWin++;
+			else iyWin++;
+			return;
+		}
+		
+		for (int i = 0; i < 9; i++) {
+			if (visited[i]) continue;
+			
+			visited[i] = true;
+			
+			int sum = gyCard[depth] + iyCard[i];
+			if (gyCard[depth] > iyCard[i]) dfs(depth + 1, gyScore + sum, iyScore);
+			else dfs(depth + 1, gyScore, iyScore + sum);
+			
+			visited[i] = false;
+		}
+	}
+
+}
