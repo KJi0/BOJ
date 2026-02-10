acb88d178202497659b1833a4e92174ec76ca061
Jiyoung Kwak <165020718+KJi0@users.noreply.github.com>
2026-02-10 14:34:25 +0900

    260210

--- SWEA/SWEA_1949.java
+++ SWEA/SWEA_1949.java
@@ -1,10 +1,6 @@
 package SWEA;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 /*
  * SWEA 1949: 등산로 조성
@@ -13,7 +9,7 @@ import java.util.StringTokenizer;
 public class SWEA_1949 {
 	static int N, K, longest;
 	static int[][] map;
-	static ArrayList<int[]> tallest;
+	static boolean[][] visited;
 	
 	static int[] dx = {-1, 1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
@@ -27,29 +23,59 @@ public class SWEA_1949 {
 			N = Integer.parseInt(st.nextToken()); // 지도 한 변 길이
 			K = Integer.parseInt(st.nextToken()); // 최대 공사 가능 깊이
 			
-			tallest = new ArrayList<>();
-			longest = Integer.MIN_VALUE;
-			
 			map = new int[N][N];
+			visited = new boolean[N][N];
+			longest = 0;
+			
+			int max = 0;
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
-					
-					//tallest = Math.max(tallest, map[i][j]);
+					max = Math.max(max, map[i][j]);
 				}
 			}
 			
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (map[i][j] == max) {
+						visited[i][j] = true;
+						dfs(i, j, 1, max, false);
+						visited[i][j] = false;
+					}
+				}
+			}
 			
+			System.out.println("#" + t + " " + longest);
 		}
 	}
 
-	static void dfs(int depth, int length) {
-		if (depth == K) {
-			longest = Math.max(longest, length);
-			return;
+	static void dfs(int x, int y, int length, int curHeight, boolean worked) {
+		longest = Math.max(longest, length);
+		
+		for (int d = 0; d < 4; d++) {
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			
+			if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+			if (visited[nx][ny]) continue;
+			
+			// 안 깎는 경우
+			if (curHeight > map[nx][ny]) {
+				visited[nx][ny] = true;
+				dfs(nx, ny, length + 1, map[nx][ny], worked);
+				visited[nx][ny] = false;
+			}
+			
+			// 깎는 경우
+			else if (!worked && curHeight > (map[nx][ny] - K)) {
+				int origin = map[nx][ny];
+				map[nx][ny] = curHeight - 1;
+				visited[nx][ny] = true;
+				dfs(nx, ny, length + 1, map[nx][ny], true);
+				visited[nx][ny] = false;
+				map[nx][ny] = origin;
+			}
 		}
-		
-		
 	}
 }
